345cb6e4f747ec4bd64a3d5a46f20123e7beda8b
Henrique Doro <opensource@hdoro.dev>
2021-11-25 10:44:50 -0300

    style: remove custom styles in favor of Sakura.css

--- template/web/src/lib/Header.svelte
+++ template/web/src/lib/Header.svelte
@@ -1,75 +1,50 @@
 <script context="module">
-  import { page } from '$app/stores'
+	import { page } from '$app/stores';
 </script>
-<script>
-  export let blogTitle = 'My Blog';
 
-  let path;
-  $:({ path } = $page);
+<script>
+	export let blogTitle = 'My Blog';
+
+	let path;
+	$: ({ path } = $page);
 </script>
 
 <header>
-  <span>{blogTitle}</span>
-  <nav>
-    <ul>
-      <li><a class='{path === "/" ? "selected" : ""}' href='/'>home</a></li>
-    </ul>
-  </nav>
+	<span>{blogTitle}</span>
+	<nav>
+		<ul>
+			<li><a data-selected={path === '/'} href="/">home</a></li>
+		</ul>
+	</nav>
 </header>
 
 <style>
-  header {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: flex-start;
-    padding: 1em;
-  }
-  header > span {
-    font-size: 1.2rem;
-  }
-  nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding-right: 1em;
-	}
 	ul {
+		list-style: none;
 		margin: 0;
 		padding: 0;
 	}
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
-	li {
-		display: block;
-		float: left;
-	}
-	.selected {
-		position: relative;
-		display: inline-block;
-	}
-	.selected::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: #ff3e00;
-		display: block;
-		bottom: -1px;
+	a[data-selected='true'] {
+		font-weight: 600;
+		color: #555;
 	}
 	a {
 		text-decoration: none;
-		padding: 1em 0.5em;
 		display: block;
+		border-bottom: 2px solid transparent;
 	}
 
-  @media screen and (min-width: 400px){
-    header {
-      flex-direction: row-reverse;
-      align-items: center;
-    }
-  }
+	header {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: flex-start;
+		padding: 1em;
+	}
+	@media screen and (min-width: 400px) {
+		header {
+			flex-direction: row-reverse;
+			align-items: center;
+		}
+	}
 </style>
--- template/web/src/routes/__layout.svelte
+++ template/web/src/routes/__layout.svelte
@@ -1,44 +1,10 @@
 <script>
-  import Header from "$lib/Header.svelte";
-  import Footer from "$lib/Footer.svelte";
+	import Header from '$lib/Header.svelte';
+	import Footer from '$lib/Footer.svelte';
 </script>
 
-<Header blogTitle={'SvelteKit x Sanity Blog'}/>
-<main>
-  <slot/>
+<Header blogTitle={'SvelteKit x Sanity Blog'} />
+<main style="padding: 1em">
+	<slot />
 </main>
 <Footer />
-
-<style>
-  main {
-    padding: 0 2rem;
-  }
-
-  :global(body) {
-    margin: 0;
-    font-family: Roboto, -apple-system, BlinkMacSystemFont, Segoe UI, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-    font-size: 14px;
-    line-height: 1.5;
-    color: #333;
-  }
-
-  :global(h1, h2, h3, h4, h5, h6) {
-    margin: 0 0 0.5em 0;
-    font-weight: 400;
-    line-height: 1.2;
-  }
-
-  :global(h1) {
-    font-size: 2em;
-  }
-
-  :global(a) {
-    color: inherit;
-  }
-
-  @media (min-width: 400px) {
-    :global(body) {
-      font-size: 16px;
-    }
-  }
-</style>
