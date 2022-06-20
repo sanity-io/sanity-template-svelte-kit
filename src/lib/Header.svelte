5f8591924352ca8544172644dccf1c9e8758e317
Austin McPhail <austin@mcphail.dev>
2022-06-20 00:07:07 -0600

    feat: 🎸 mobile menu

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,4 +1,6 @@
-<script></script>
+<script>
+  let menuOpen = false
+</script>
 
 <header>
   <a href="/" id="logo">
@@ -10,7 +12,22 @@
     <li><a href="/about">Gallery</a></li>
     <li>&bull;</li>
     <li><a href="/contact">Pricing</a></li>
+
+    <li id="mobile-trigger">
+      <button on:click={() => (menuOpen = !menuOpen)}> Menu </button>
+    </li>
   </ul>
+
+  <div id="mobile-menu" class:open={menuOpen}>
+    <a href="/">
+      <img src="header-logo.png" alt="Kelsey Lea Photography" />
+    </a>
+    <a href="/">Home</a>
+    <a href="/about">Gallery</a>
+    <a href="/contact">Pricing</a>
+
+    <button on:click={() => (menuOpen = !menuOpen)}> Close </button>
+  </div>
 </header>
 
 <style>
@@ -74,5 +91,62 @@
   a {
     text-decoration: none;
     color: inherit;
+  }
+
+  li {
+    display: none;
+  }
+  #mobile-trigger {
+    display: inline-block;
+  }
+
+  #mobile-menu {
+    position: absolute;
+    transform: translate3d(0, -100%, 0);
+    transition: transform 250ms ease-out;
+    left: 0;
+    width: 100%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    padding: 1rem 0rem 1rem 0rem;
+
+    background: var(--dark);
+    border-bottom: 2px var(--light) solid;
+
+    color: var(--light);
+  }
+
+  #mobile-menu button {
+    align-self: flex-end;
+    margin-right: 1rem;
+  }
+
+  #mobile-menu.open {
+    position: absolute;
+    transform: translate3d(0, 0, 0);
+    top: 0;
+  }
+  @media (min-width: 768px) {
+    #mobile-menu {
+      display: none;
+    }
+    li {
+      display: inline-block;
+    }
+    #mobile-trigger {
+      display: none;
+    }
+  }
+
+  button {
+    background: none;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    color: var(--light);
   }
 </style>
