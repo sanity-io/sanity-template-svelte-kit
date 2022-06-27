f862e82ddf36d31b509f97bdcf871285407d73f5
Austin McPhail <austin@mcphail.dev>
2022-06-27 07:10:28 -0600

    fix: 🐛 remove old

--- src/routes/pricing/[pack]/[pack].json.js
+++ src/routes/pricing/[pack]/[pack].json.js
@@ -1,28 +1,7 @@
-import {AUTHOR_CARD_FRAGMENT} from '$lib/queries'
-import {client} from '$lib/sanityClient'
+// import {client} from '$lib/sanityClient'
 
 // Gets a specific blog post from its slug.current value
 export async function get({params: {pack}}) {
-  // const post = await client.fetch(/* groq */ `*[_type == "post" && slug.current == "${slug}"][0]{
-  //   ...,
-  // 	"authors": authors[].author->{
-  // 		${AUTHOR_CARD_FRAGMENT}
-  // 	},
-  //   body[] {
-  //     ...,
-  // 		children[] {
-  // 			...,
-  // 			// authorReference is an inline block, and will show inside PortableText spans (block.children)
-  // 			// Let's expand the reference to the author document & get its name, slug & image
-  // 			_type == "authorReference" => {
-  // 				author->{
-  // 					${AUTHOR_CARD_FRAGMENT}
-  // 				}
-  // 			}
-  // 		}
-  //   }
-  // }`)
-
   let name = ''
   let previewImageOne = ''
   let previewImageTwo = ''
